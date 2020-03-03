98b9fe6e85a926a2c4c3e5c99590b5cb2ad502b4
Admin <belyakov.td@edu.spbstu.ru>
2020-03-03 15:15:43 +0300

    Lesson 1: all tasks are solved;

--- test/lesson1/task1/Tests.kt
+++ test/lesson1/task1/Tests.kt
@@ -94,9 +94,9 @@ class Tests {
     @Test
     @Tag("Easy")
     fun accountInThreeYears() {
-        assertEquals(133.1, accountInThreeYears(100, 10), 1e-2)
-        assertEquals(1.0, accountInThreeYears(1, 0), 1e-2)
-        assertEquals(104.0, accountInThreeYears(13, 100), 1e-2)
+        assertEquals(133.1, accountInThreeYears(100.0, 10.0), 1e-2)
+        assertEquals(1.0, accountInThreeYears(1.0, 0.0), 1e-2)
+        assertEquals(104.0, accountInThreeYears(13.0, 100.0), 1e-2)
     }
 
     @Test
